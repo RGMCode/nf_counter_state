84e1812d6f9303147d2ce74a0e0154529e4d5d65
mrg <rgmengedoth@gmail.com>
2023-07-27 11:13:32 +0200

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+    function Counter() {
+        const [count, setCount] = useState(0)
+        return (
+            <>
+                <div className="card">
+                    <h1>Counter: {count}</h1>
+                    <button className={"btnCount1"}
+                            onClick={() => setCount((count) => count + 1)
+                    }>+</button>
+                    <button className={"btnCount2"}
+                            onClick={() => {
+                                if(count > 0) {
+                                    setCount((count) => count - 1)
+                                }
+                            }
+                    }>-</button>
+                </div>
+            </>
+        )
+    }
 
-  return (
-    <>
-      <div className="card">
 
-            <h1>Counter: {count}</h1>
+    function App() {
 
-            <button className={"btnCount"}
-                    onClick={() => setCount((count) => count + 1)
-            }>+</button>
-
-            <button className={"btnCount"}
-                    onClick={() => setCount((count) => count - 1)
-            }>-</button>
-
-      </div>
-    </>
-  )
-}
+      return (
+            <div>
+                <Counter></Counter>
+            </div>
+      )
+    }
 
 export default App
