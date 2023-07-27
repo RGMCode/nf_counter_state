95bd59efdffe922aaa3b839c7d15248c89c05840
mrg <rgmengedoth@gmail.com>
2023-07-27 11:51:45 +0200

    push css

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 
-    function Counter() {
-        const [count, setCount] = useState(0)
+    type ButtonProps = {
+        intNum: number
+    }
+
+    function Counter( {intNum}: ButtonProps ) {
+        const [count, setCount] = useState(intNum)
         return (
             <>
                 <div className="card">
                     <h1>Counter: {count}</h1>
                     <button className={"btnCount1"}
-                            onClick={() => setCount((count) => count + 1)
-                    }>+</button>
+                            onClick={() => setCount(count + 1)}>+</button>
                     <button className={"btnCount2"}
                             onClick={() => {
                                 if(count > 0) {
-                                    setCount((count) => count - 1)
+                                    setCount(count - 1)
                                 }
-                            }
-                    }>-</button>
+                            }}>-</button>
                 </div>
             </>
         )
     }
 
 
+
     function App() {
 
       return (
             <div>
-                <Counter></Counter>
+                <Counter intNum={0}></Counter>
+                <Counter intNum={7}></Counter>
+                <Counter intNum={13}></Counter>
             </div>
       )
     }
